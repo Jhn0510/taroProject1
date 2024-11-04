b49e70b64ecf521e92f4d300c3e17292a32e96f5
Jhn0510 <2097391824@qq.com>
2024-11-04 15:37:15 +0800

    首⻚公告

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,12 +1,29 @@
+import { getIndexNotice } from '@/service/notice';
+import { useEffect, useState} from 'react';
 import { View } from '@tarojs/components';
+import Notice from './components/Notice';
 import './index.scss';
 import Navbar from './components/CustomNavbar';
 
+
 export default function Index() {
+  const [noticeList, setNoticeList] = useState<SwiperItem[]>([]);
+  const [swiper, setSwiper] = useState<SwiperItem[]>([]);
+
+  const getIndexNoticeFunction = async () => {
+    const res = await getIndexNotice();
+    if (res.code === 0) {
+      setNoticeList(res.data);
+    }
+  };
+  useEffect(() => {
+    getIndexNoticeFunction();
+  }, []);
 
   return (
     <View className='index'>
       <Navbar />
+      <Notice noticeList={noticeList} />
     </View>
   );
 }
